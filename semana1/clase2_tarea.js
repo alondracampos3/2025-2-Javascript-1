a208609223f4485dbd5db0e8a9ec32cbf118b7b1
Alondra <alondra_caflo@hotmail.com>
2025-05-19 19:33:35 -0600

    hola

--- semana1/clase2_tarea.js
+++ semana1/clase2_tarea.js
@@ -12,10 +12,8 @@ const { ask } = require('../helpers/input');
 async function main() {
   const temperatura = Number(await ask("Alexa, ¿Cuál es la temperatura actual?"));
 
-  if (temperatura < 10) {
+if (temperatura < 10) {
     console.log("Hace frío");
-  } else if (temperatura >=10 && temperatura < 20) {
-    console.log("Está templado");
   }
   else if (temperatura >= 20 && temperatura < 30) {
     console.log("Hace calor");
--- semana2/clase1_tarea.js
+++ semana2/clase1_tarea.js
@@ -4,6 +4,17 @@ const { ask } = require('../helpers/input');
 
 async function main() {
 
+    const num1 = Number(await ask('Ingresa el primer número: '));
+    const num2 = Number(await ask('Ingresa el segundo número menor a 100: '));
+    
+    for (let i = num1; i <= num2; i++) {
+        if (i % 2 === 0) {
+        console.log(`El número ${i} es par`);
+        }
+    }
+
+    
+
 }
 
 main();
